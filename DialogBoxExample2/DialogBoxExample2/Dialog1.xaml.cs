9da8eeddf2f084e8eaf692af686fdcdbd9780aff
ALPAXMX S de R.L. de C.V <contacto@alpaxmx.com>
2018-11-05 15:28:54 -0600

    ejemplo para como mostrar una pagina estilo dialogo modal que pida 3 botones y como obtener los resultados despues

---
+++ DialogBoxExample2/DialogBoxExample2/Dialog1.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DialogBoxExample2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Dialog1 : ContentPage
+    {
+        public Dialog1ViewModel vm;
+
+        protected override Boolean OnBackButtonPressed()
+        {
+            return true;
+        }
+        public Dialog1()
+        {
+            InitializeComponent();
+            vm = new Dialog1ViewModel();
+            BindingContext = vm;
+
+            btnOpcion1.Clicked +=async (e, a) =>
+              {
+                  vm.NumOpcionSeleccionada = 1;
+                  await Navigation.PopModalAsync();
+              };
+
+            btnOpcion2.Clicked += async (e, a) =>
+            {
+                vm.NumOpcionSeleccionada = 2;
+                await Navigation.PopModalAsync();
+            };
+
+            btnOpcion3.Clicked += async (e, a) =>
+            {
+                vm.NumOpcionSeleccionada = 3;
+                await Navigation.PopModalAsync();
+            };
+
+       
+
+    }
+    }
+
+    public class Dialog1ViewModel
+    {
+        public EventHandler<Int32> OpcionSeleccionada;
+        private Int32 numOpcionSeleccionada;
+        public Int32 NumOpcionSeleccionada
+        {
+            get { return numOpcionSeleccionada;  }
+            set
+            {
+                numOpcionSeleccionada = value;
+                EventHandler<Int32> handler = OpcionSeleccionada;
+                if (handler != null)
+                {
+                    handler(this, numOpcionSeleccionada);
+                }
+            }
+        }
+
+        public Dialog1ViewModel()
+        {
+            numOpcionSeleccionada = 0;
+        }
+
+     
+    }
+}
---
+++ DialogBoxExample2/DialogBoxExample2/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DialogBoxExample2
+{
+    public partial class MainPage : ContentPage
+    {
+         
+        public MainPage()
+        {
+            InitializeComponent();
+
+       
+            this.btnMain.Clicked +=async (e, a) =>
+            {
+                Dialog1 dlg = new Dialog1();
+                dlg.vm.OpcionSeleccionada += (s, p) =>
+                {
+                    this.lblResultado.Text = "La opción seleccionada fue la " +  p.ToString();
+                };
+
+                await Navigation.PushModalAsync(dlg);
+
+     
+                
+            };
+        }
+
+     
+    }
+}
